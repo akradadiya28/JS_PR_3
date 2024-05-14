54a5772fafd696d2704847cd5ed176ee2799db20
akradadiya28 <akradadiya28@gmail.com>
2024-05-14 11:46:20 +0530

    task-5

---
+++ task-5/js/script.js
@@ -0,0 +1,16 @@
+// Write a JAVASCRIPT Program to calculate the Fibonacci Series of a given number using while loop.
+
+let number = 100;
+let x = 0;
+let y = 1;
+
+let fibonacciSerise = x + y;
+
+document.getElementById("fn").innerHTML += (`${x} </br>`);
+
+while (fibonacciSerise < number) {
+    document.getEleme`ntById("fn").innerHTML += (`${ fibonacciSerise } </br > `);
+    fibonacciSerise = x + y;
+    x = y;
+    y = fibonacciSerise;
+} 
